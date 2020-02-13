e5b2c46f7d3efe9d014d926dfaffc4e27b79b86b
Marcos Paixao <marcos.paixao@iatec.com>
2020-02-13 10:21:42 -0300

    primeiro comit do projeto

---
+++ Solidarius/Data/SolidariusContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Solidarius.Models
+{
+    public class SolidariusContext : DbContext
+    {
+        public SolidariusContext (DbContextOptions<SolidariusContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Solidarius.Models.Account> Account { get; set; }
+    }
+}
---
+++ Solidarius/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Solidarius.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
